d4229ede001d2baa889f0bc458211fd0e3e6adce
PRAGLEND21 <praglend21@gcc.edu>
2022-02-23 14:49:23 -0500

    ur mom compatiablez

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         Scanner scanuel = new Scanner(System.in);
         System.out.println("What is your name?");
-        String name = scanuel.next();
-        System.out.println("Hello " + name);
+        String name = scanuel.nextLine();
+        Scanner scanly = new Scanner(name);
+        String finalname = "";
+        while(scanly.hasNext()){
+            finalname += scanly.next();
+            finalname += " ";
+        }
+        System.out.println("Hello " + finalname);
     }
 }

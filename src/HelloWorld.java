0ea76417b4cafb5c725383ce491c73059ebc11a2
DcWhm05 <willsegs@gmail.com>
2022-02-23 14:34:27 -0500

    haha i changed your code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanuel = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanuel.next();
+        System.out.println("Hello " + name);
     }
 }
